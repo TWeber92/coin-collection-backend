f13050c4ca0d3808e676f672601b8eae1cb318c1
TWeber92 <tadweber92@gmail.com>
2026-01-24 17:39:20 -0700

    Corrected format index.js

--- coin-collection-proxy/functions/index.js
+++ coin-collection-proxy/functions/index.js
@@ -1,25 +1,27 @@
 import { handler as getCoin } from "./getCoin.js";
 import { handler as postAll } from "./postAll.js";
 
-export async function fetch(request, env) {
-  const url = new URL(request.url);
-  const event = {
-    path: url.pathname,
-    httpMethod: request.method,
-    queryStringParameters: Object.fromEntries(url.searchParams),
-    body: request.method === "POST" ? await request.text() : null,
-  };
-  const routerMap = {
-    "GET:/api/coin": async () => await getCoin(event, env),
-    "POST:/api/coins": async () => await postAll(event, env),
-  };
-  const router = routerMap[`${request.method}:${url.pathname}`];
-  if (!router) {
-    return new Response("Not Found", { status: 404 });
-  }
-  const response = await router();
-  return new Response(response.body, {
-    status: response.statusCode,
-    headers: response.headers,
-  });
-}
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const event = {
+      path: url.pathname,
+      httpMethod: request.method,
+      queryStringParameters: Object.fromEntries(url.searchParams),
+      body: request.method === "POST" ? await request.text() : null,
+    };
+    const routerMap = {
+      "GET:/api/coin": async () => await getCoin(event, env),
+      "POST:/api/coins": async () => await postAll(event, env),
+    };
+    const router = routerMap[`${request.method}:${url.pathname}`];
+    if (!router) {
+      return new Response("Not Found", { status: 404 });
+    }
+    const response = await router();
+    return new Response(response.body, {
+      status: response.statusCode,
+      headers: response.headers,
+    });
+  },
+};
