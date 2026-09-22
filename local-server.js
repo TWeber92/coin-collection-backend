f89dd356d558c80cb185e817198eadf49a76b39d
Tad Weber <tadweber92@gmail.com>
2026-09-22 16:49:51 -0700

    Created local env, pushing to test gitignore file

--- local-server.js
+++ local-server.js
@@ -1,18 +1,23 @@
-// import { createServer } from "node:https";
-// import { readFileSync } from "node:fs";
+import { createServer } from "node:https";
+import { readFileSync } from "node:fs";
 
-// const PORT = 3000;
+const env = {
+  OORT_ACCESS_KEY: process.env.OORT_ACCESS_KEY,
+  OORT_SECRET_KEY: process.env.OORT_SECRET_KEY,
+  OORT_BUCKET: process.env.OORT_BUCKET,
+};
+const PORT = 3000;
 
-// const options = {
-//   key: readFileSync("./certs/key.pem"),
-//   cert: readFileSync("./certs/cert.pem"),
-// };
+const options = {
+  key: readFileSync("./certs/key.pem"),
+  cert: readFileSync("./certs/cert.pem"),
+};
 
-// const server = createServer(options, (req, res) => {
-//   res.writeHead(200, { "Content-Type": "text/plain" });
-//   res.end("Hello from the local HTTPS server\n");
-// });
+const server = createServer(options, (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from the local HTTPS server\n");
+});
 
-// server.listen(PORT, () => {
-//   console.log(`Listening on https://localhost:${PORT}`);
-// });
+server.listen(PORT, () => {
+  console.log(`Listening on https://localhost:${PORT}`);
+});
